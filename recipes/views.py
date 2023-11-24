a2c39f725a8424f7d7a1fc10cff07e1aeeb365c9
marchayomo <marcelo.hayami@gmail.com>
2023-11-24 16:30:09 -0300

    commit 3

---
+++ recipes/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('HOME 2')
+
+
+def contato(request):
+    return HttpResponse('contato')
+
+
+def sobre(request):
+    return HttpResponse('sobre')
