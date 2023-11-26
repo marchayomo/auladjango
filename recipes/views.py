2f2d66df7539332bd27fb70e0efb8fa68892632f
marchayomo <marcelo.hayami@gmail.com>
2023-11-26 17:02:32 -0300

    commit 8

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name':'Marcelo',
+        'recipes': [make_recipe() for _ in range(10)],
     })
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name':'Marcelo',
+        'recipe': make_recipe(),
     })
