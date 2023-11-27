da16094d077b0894d78e6a54fcb8de632d20ebd1
marchayomo <marcelo.hayami@gmail.com>
2023-11-27 17:27:17 -0300

    commit 10

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Category, Recipe
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+admin.site.register(Category, CategoryAdmin)
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    ...
