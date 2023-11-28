19f253a40cdf3f475398f8743da3538cb0de316b
marchayomo <marcelo.hayami@gmail.com>
2023-11-28 11:01:24 -0300

    commit12

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render, get_list_or_404
-from utils.recipes.factory import make_recipe
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from recipes.models import Recipe
 
 
 def home(request):
     recipes = Recipe.objects.filter(
-        is_published=True
+        is_published=True,
     ).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
@@ -22,7 +21,8 @@ def category(request, category_id):
     })
 
 def recipe(request, id):
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True,)
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True,
     })
