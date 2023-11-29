50592dee2014eb2eeb87008c083404d5d7ae0e51
marchayomo <marcelo.hayami@gmail.com>
2023-11-29 17:19:20 -0300

    commit testes3

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -113,3 +113,12 @@ class RecipeViewsTest(RecipeTestBase):
         content = response.content.decode('utf-8')
 
         self.assertEqual(response.status_code, 404)
+
+    def test_recipe_search_uses_correct_view_function(self):
+        resolved = resolve(reverse('recipes:search'))
+        self.assertIs(resolved.func, views.search)
+
+    def test_recipe_search_loads_correct_template(self):
+        response = self.client.get(reverse('recipes:search'))
+        self.assertTemplateUsed(response, 'recipes/pages/search.html')
+
